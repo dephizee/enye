ebcecb0bdc1bf02ca9ebba52851c1a439d75a4ef
dephizee <hafiz227@gmail.com>
2021-02-16 21:18:56 +0100

    d

--- routes/api_call.js
+++ routes/api_call.js
@@ -28,30 +28,6 @@ router.get('/rates', async function (req, res, next)  {
   }
 });
 
-router.get('/rates_my_server', async function (req, res, next)  {
-  try {
 
-	var query = req.query;
-    console.log(query);
-    const apiResponse = await fetch(
-      `https://api.exchangeratesapi.io/latest?base=${query.base}` 
-    )
-    const apiResponseJson = await apiResponse.json()
-    var rates = apiResponseJson.rates;
-    var currencies = query.currency.split(",");
-    apiResponseJson.rates = Object.keys(rates)
-		  .filter(key => currencies.includes(key))
-		  .reduce((obj, key) => {
-		    obj[key] = rates[key];
-		    return obj;
-		  }, {});
-    
-    console.log(apiResponseJson)
-    res.send(apiResponseJson)
-  } catch (err) {
-    console.log(err)
-    res.status(500).send('Something went wrong')
-  }
-});
 
 module.exports = router;
